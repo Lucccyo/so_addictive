e31f7843621b58e17d58737618039000c2539bce
Lucccyo <cha.git@mailo.fr>
2023-07-21 17:21:04 +0200

    change ocaml functions

--- src/addition.ml
+++ src/addition.ml
@@ -1,6 +1,25 @@
-let add x y = x + y
-let dooble x = x * 2
+let sum x y = x + y
 
-let _ = Callback.register "ocaml_add" add
-let _ = Callback.register "ocaml_dooble" dooble
+let rec sum_two_list la lb =
+  match la with
+  | [] -> lb
+  | hda :: tla ->
+    match lb with
+    | [] -> la
+    | hdb :: tlb -> (hda + hdb) :: sum_two_list tla tlb
+
+let rec add_to_list l n =
+  match l with
+  | [] -> []
+  | hd :: tl -> hd + n :: add_to_list tl n
+
+let rec sum_list l =
+  match l with
+  | [] -> 0
+  | hd :: tl -> hd + sum_list tl
+
+let _ = Callback.register "ocaml_sum" sum
+let _ = Callback.register "ocaml_sum_two_list" sum_two_list
+let _ = Callback.register "ocaml_add_to_list" add_to_list
+let _ = Callback.register "ocaml_sum_list" sum_list
 
--- src/main.c
+++ src/main.c
@@ -6,15 +6,15 @@
 
 
 
-int add(int x, int y){
-  static const value * closure_add = NULL;
+int sum(int x, int y){
+  static const value * closure_sum = NULL;
   CAMLparam0();
-  CAMLlocal1(ocaml_res_add);
-  if (closure_add == NULL) {
-    closure_add = caml_named_value("ocaml_add");
+  CAMLlocal1(ocaml_res_sum);
+  if (closure_sum == NULL) {
+    closure_sum = caml_named_value("ocaml_sum");
   }
-  ocaml_res_add = caml_callback2(*closure_add, Val_int(x), Val_int(y));
-  CAMLreturn(ocaml_res_add);
+  ocaml_res_sum = caml_callback2(*closure_sum, Val_int(x), Val_int(y));
+  CAMLreturn(ocaml_res_sum);
 }
 
 int main(int argc, char **argv) {
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
   int x = 3;
   int y = 7;
   printf("x = %d\ty = %d\n", x, y);
-  int z = add(x, y);
+  int z = sum(x, y);
   printf("x + y by OCaml = %d\n", z);
   return 0;
 }
